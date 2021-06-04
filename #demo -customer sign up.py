73f6703571f52b3ffb268d724582a7b678659d75
iajalil <iajalil98@gmail.com>
2021-06-04 21:17:55 +0000

    work on button

---
+++ #demo -customer sign up.py
@@ -0,0 +1,55 @@
+#importing tkinter for our interface design
+
+import tkinter as tk #using the alias tk instead of the normal tkinter
+from tkinter import ttk
+
+win = tk.Tk()
+
+#Giving the window a title
+win.title('customer sign up form')
+
+#creating an empty space
+ttk.Label(win).grid(row=0)
+
+#Setting our username labels and input boxes
+lblUserName = ttk.Label (win, text = "Username:")
+lblUserName.grid(column=1 , row= 1)
+
+#creating an empty space
+ttk.Label(win).grid (row=2)
+
+#setting or username textbox
+name=tk.StringVar()
+txtUsername = ttk.Entry(win, width= 16, textvariable = name) 
+txtUsername.grid(column =3, row =1)
+
+#setting our email labels and input boxes
+lblEmail = ttk.Label(win,text = "Email: ")
+lblEmail.grid(column=1, row=3)
+
+#setting our email textbox
+email = tk.StringVar()
+txtEmail = ttk.Entry(win, width=25, textvariable= email)
+txtEmail.grid(column=3, row=3)
+
+#creating an empty space
+ttk.Label(win).grid(row=4)
+
+#setting our password labels and input boxes
+lblPassword = tk.Label(win, text ="Password")
+lblPassword.grid(column=1, row=5)
+
+#setting our password text box
+password = tk.StringVar()
+txtPassword =ttk.Entry(win, width=20, textvariable=password)
+txtPassword.grid(column=3, row=5)
+
+#Creating an empty space
+tk.Label(win).grid(row=6)
+
+#setting up our submit button
+btnSubmit =ttk.Button(win, width= 15, text="submit ")
+btnSubmit.grid(column=3, row=7)
+
+#Invoking our GUI loop
+win.mainloop()
--- window.py
+++ window.py
@@ -10,7 +10,7 @@ window = tk.Tk()
 window.title( "our demo app" )
 
 # restricting window resizing
-window.resizable( True, True)
+window.resizable( True, True) #x and y
 
 # introducing our first label
 ttk.Label(window, text = "Guess my name"). grid(column =0, row = 0)
@@ -18,14 +18,33 @@ lblName =ttk.Label(window, text ="Guess my name")
 lblName.grid(column=0, row = 0)
 
 # Starting our buton up for an event
-def reveal_me():
-    action.configure( text = "Mystery solved") #Text that replaces "Guess my name"
+def reveal_me ():
+    action.configure( text = "Mystery solved!") #Text that replaces "Guess my name"
     lblName.configure(foreground ="blue") #colour of the text
     lblName.configure(text = "Benjamin Benson") #name revealed
 
+# starting up our textfield event
+def click_me():
+    text_action.configure( text = "hello " + name.get()) # Displaying user's name
+
+#setting our text label
+lblText = ttk.Label( window, text = "please enter a name" )
+lblText.grid( column = 0, row = 2)
+
+ #Adding the textbox widget
+name = tk.StringVar ()
+user_name = ttk.Entry ( window, width = 14, textvariable=name)
+user_name.grid(column=0, row=3)
+user_name.focus() # placing the cursor to focus on this texting
+
 # Adding our button
-action = ttk. Button( window, text = "open Sesame!", command = reveal_me) #Activating the button
-action.grid( column = 1, row = 0)
+action = ttk.Button(window, text="open sesame!", command=reveal_me) # Activating the button
+action.grid(column=1, row =0 )
+action.configure(state = "disabled") # Disabling the button
+
+#Adding our text buton
+text_action = ttk.Button(window, text = "click to greet" , command = click_me)
+text_action.grid( column=1, row = 3)
 
 # starting the GUI
 window.mainloop()
